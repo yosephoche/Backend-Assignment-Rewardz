5cfecc970d31f6d3b7950e1429246f5ba27431c8
Yoseph Parai <yosephoche@gmail.com>
2022-09-10 14:41:28 +0800

    add user model and migrations file

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,57 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin, Group
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, **kwargs):
+        account = self.model(
+            email=self.normalize_email(kwargs.get('email')),
+            username=kwargs.get('email')
+        )
+
+        account.set_password(kwargs.get('password'))
+        account.save()
+
+        return account
+
+    def create_superuser(self, **kwargs):
+        account = self.create_user(**kwargs)
+        account.is_staff = True
+        account.is_superuser = True
+        account.is_active = True
+        account.save()
+
+        return account
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(unique=True, max_length=500)
+
+    first_name = models.CharField(max_length=500)
+    last_name = models.CharField(max_length=500)
+    unique_id = models.CharField(max_length=500, default=uuid.uuid4, editable=False, unique=True)
+
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    admin_panel_access = models.BooleanField(default=False)
+
+    verify = models.BooleanField(default=True)
+
+    profile_image = models.ImageField(upload_to='profile_image', blank=True, null=True)
+
+    # company_list = ArrayField(models.IntegerField(null=True, default=None), size=10)
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def get_fullname(self):
+        return '%s %s' % (self.first_name, self.last_name)
