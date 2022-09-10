0ed45f85d270b4ebd5e0fec6bee653d6f7f7c9ef
Yoseph Parai <yosephoche@gmail.com>
2022-09-10 14:53:43 +0800

    add book model and migration file

--- book/models.py
+++ book/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from library_system.behaviors import Timestampable
+
+
+class Book(Timestampable, models.Model):
+    title = models.CharField("Title", max_length=200)
+    author = models.CharField("Author", max_length=100)
+    publisher = models.CharField("Publisher", max_length=100)
+    year = models.CharField("Year", max_length=50)
+    description = models.TextField()
+    qty = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title
+
+
+class BookAwareModel(Timestampable, models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
